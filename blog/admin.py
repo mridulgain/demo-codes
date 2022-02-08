f2daf27a9f0de46277a2ddccb2cc4642d5f17e7c
Rohan <mridulgain@gmail.com>
2022-02-08 08:04:05 +0530

    chap2: comment section added

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,7 @@
-from datetime import date
 from django.contrib import admin
-from .models import Post
+from .models import Comment, Post
 # Register your models here.
-# admin.site.register(Post)
+# admin.site.register(Comment)
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -13,3 +12,9 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
     # raw_id_fields = ('author',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,38 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
 from django.core.mail import send_mail
+from .forms import EmailPostForm, CommentForm
+
 
 # Create your views here.
 def post_details(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
-        publish__month=month, publish__day=day)
+    post = get_object_or_404(Post, slug=post, status='published', 
+        publish__year=year, publish__month=month, publish__day=day)
     
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'blog/post/detail.html', 
+        {'post': post,
+        'comments': comments,
+        'new_comment': new_comment,
+        'comment_form': comment_form,})
 
 def post_list(request):
     post_objs = Post.published.all()
@@ -23,8 +46,6 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts':posts, 'page':page})
 
-
-from .forms import EmailPostForm
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
