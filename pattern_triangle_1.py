7d2b63a9c4dc474fcd79e020a8f15b1bca480e99
Mridul Gain <mridulgain@gmail.com>
2024-03-13 16:18:36 +0530

    Update and rename pattern1.py to pattern_triangle_1.py

--- pattern_triangle_1.py
+++ pattern_triangle_1.py
@@ -1,9 +1,16 @@
 # left to right top down
-lines = int(input("? "))
+lines = int(input("rows? "))
 
 for i in range(1,lines+1):
     # for each line
     for j in range(i):
         print("*", end="")
     print("")
-    
+
+'''
+rows? 4
+*
+**
+***
+****
+'''
