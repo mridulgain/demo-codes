910a89a323499c1b413b112eb2a97257c2290f3a
Mridul Gain <mridul@platform9.com>
2026-04-09 11:57:11 +0530

    dynamic programming: fibonacci

---
+++ go/dp/fib.go
@@ -0,0 +1,43 @@
+package dp
+
+func fib_recurr(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fib_recurr(n-1) + fib_recurr(n-2)
+}
+
+func fib_memo(n int) int {
+	dp := make([]int, n+1)
+	func() {
+		dp[0] = 0
+		dp[1] = 1
+		for i := 2; i <= n; i++ {
+			dp[i] = -1
+		}
+	}()
+
+	var helper func(int) int
+	helper = func(n int) int {
+		if n <= 1 {
+			return dp[n]
+		}
+		if dp[n] != -1 {
+			return dp[n]
+		}
+		dp[n] = helper(n-1) + helper(n-2)
+		return dp[n]
+	}
+
+	return helper(n)
+}
+
+func fib_tabulation(n int) int {
+	dp := make([]int, n+1)
+	dp[0] = 0
+	dp[1] = 1
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
---
+++ go/dp/fib_test.go
@@ -0,0 +1,98 @@
+package dp
+
+import (
+	"testing"
+	"time"
+)
+
+// go test -v ./dp -timeout=5s -run TestfibMemo
+
+type args struct {
+	n int
+}
+
+var tests = []struct {
+	name string
+	args args
+	want int
+}{
+	{
+		name: "test0",
+		args: args{
+			n: 1,
+		},
+		want: 1,
+	},
+	{
+		name: "test1",
+		args: args{
+			n: 10,
+		},
+		want: 55,
+	},
+	{
+		name: "test5",
+		args: args{
+			n: 40,
+		},
+		want: 102334155,
+	},
+	{
+		name: "test6",
+		args: args{
+			n: 45,
+		},
+		want: 1134903170,
+	},
+	{
+		name: "test7",
+		args: args{
+			n: 50,
+		},
+		want: 12586269025,
+	},
+}
+
+// skipping testcases that takes more than 1 second to complete
+func TestFibRecurr(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			done := make(chan bool)
+			var got int
+
+			go func() {
+				got = fib_recurr(tt.args.n)
+				done <- true
+			}()
+
+			select {
+			case <-done:
+				if got != tt.want {
+					t.Errorf("fib_recurr() = %v, want %v", got, tt.want)
+				}
+			case <-time.After(1 * time.Second):
+				t.Skipf("skipping test case %s: took longer than 1 second", tt.name)
+			}
+		})
+	}
+}
+
+func TestFibMemo(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fib_memo(tt.args.n); got != tt.want {
+				t.Errorf("fib_memo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFibTabulation(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fib_tabulation(tt.args.n); got != tt.want {
+				t.Errorf("fib_tabulation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
